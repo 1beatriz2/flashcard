fb83cbe247ba624a518c8e972aef322e9e5ed114
1beatriz2 <108466037+1beatriz2@users.noreply.github.com>
2024-11-27 09:20:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é o maior cachorro do mundo?',
+    'O Dogue Alemão'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Bom dia em inglês é good morning '
+)
